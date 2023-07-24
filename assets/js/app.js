44245ae58124d9f047ed8459b237805209d98ca6
Renan Soares <renansoaresffc@gmail.com>
2023-07-24 18:13:18 -0300

    feat: hero section

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+const functionality = [
+    {
+        id: 1,
+        name: 'Crie conexões',
+        description: 'Lorem ipsum dolor sit amet, consecteteu.',
+        imgUrl: 'assets/connections.svg'
+    },
+    {
+        id: 2,
+        name: '100% gratuito',
+        description: 'Lorem ipsum dolor sit amet, consecteteu.',
+        imgUrl: 'assets/free.svg'
+    },
+    {
+        id: 3,
+        name: 'Compartilhamento',
+        description: 'Lorem ipsum dolor sit amet, consecteteu.',
+        imgUrl: 'assets/sharing.svg'
+    }
+]
